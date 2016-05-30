ecae58c9e61490138f9be706d1f56a10af9ee55b
Jenny Hong <jenny.yhong@gmail.com>
2016-05-29 18:17:54 -0700

    Use multiprocessing to multithread the hyperparameter search

--- baseline_tests.py
+++ baseline_tests.py
@@ -1,8 +1,13 @@
+from multiprocessing import Pool
 import tensorflow as tf
 
 from baseline import BaselineModel, train_baseline_model, test_baseline_model
 from config import Config
 from util import reddit_data
+
+def train_baseline_thread(config, dataLoader):
+  model = BaselineModel(config, dataLoader)
+  return train_baseline_model(model)
 
 def main():
   # dataLoader is created outside, because for now, none of the 
@@ -12,9 +17,10 @@ def main():
   # If we ever do change things like LSTM size, which affect reading in 
   # the data, that should be the outermost loop, so that we have to
   # read in data as few times as possible
+  pool = Pool()
   default_config = Config()
   dataLoader = reddit_data.DataLoader(default_config)
-  with open('results.txt', 'w') as f:
+  with open('results.txt', 'a+') as f:
     hidden_sizes = [10, 20, 30, 50]
     learning_rates = [0.0001, 0.0005, 0.001, 0.005, 0.01]
     annealing = [1.0, 1.2, 1.5]
@@ -29,10 +35,12 @@ def main():
             config.anneal_by = anneal_by
             config.l2_reg = l2_reg
             with tf.Graph().as_default():
-              baselineModel = BaselineModel(config, dataLoader)
-              best_val_pp = train_baseline_model(baselineModel)
+              # baselineModel = BaselineModel(config, dataLoader)
+              # best_val_pp = train_baseline_model(baselineModel)
+              result = pool.apply_async(train_baseline_thread, [config, dataLoader])
+              best_val_acc = result.get()
               f.write(str(config) + '\n')
-              f.write('Best validation perplexity: ' + str(best_val_pp) + '\n')
+              f.write('Best validation accuracy: ' + str(best_val_acc) + '\n')
               f.write('=-=' * 5 + '\n')
               f.write('=-=' * 5 + '\n')
 
