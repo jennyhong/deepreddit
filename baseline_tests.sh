a90db2f6771cdc3676b92214f1c1cec5ad089e24
Leon Yao Yao <leonyao@madmax3.stanford.edu>
2016-05-29 20:54:30 -0700

    WIP baseline_tests madmax version

--- baseline_tests.sh
+++ baseline_tests.sh
@@ -1,3 +1,6 @@
+source $HOME/rc/tensorflow/bin/activate
+alias tfpython="LD_LIBRARY_PATH=\"$HOME/rc/my_libc_env/lib/x86_64-linux-gnu/:$HOME/rc/my_libc_env/usr/lib64/\" $HOME/rc/my_libc_env/lib/x86_64-linux-gnu/ld-2.17.so $HOME/rc/tensorflow/bin/python"
+
 for hsz in 20 30 50
 # for hsz in 20 30
 do
@@ -7,10 +10,12 @@ do
     for anneal in 1.0 1.2 1.5
     # for anneal in 1.0
     do
-      for reg in 0.001 0.01 0.1 1.0
+      # for reg in 0.001 0.01 0.1 1.0
       # for reg in 0.01 0.1
+      # for reg in 0.001 # madmax3
+      for reg in 0.01 # madmax
       do
-        python baseline_cmdline.py --hiddensize=$hsz --lr=$lr --annealby=$anneal --l2reg=$reg &
+        tfpython baseline_cmdline.py --hiddensize=$hsz --lr=$lr --annealby=$anneal --l2reg=$reg &
       done
     done
   done
